5359fb46c9fefe91050ca34101e7cae0ef599c1b
Jannis R <mail@jannisr.de>
2019-11-17 22:37:26 +0100

    queue cmd, 1.1.0

--- cli.js
+++ cli.js
@@ -31,6 +31,7 @@ if (argv.version || argv.v) {
 }
 
 const showError = (err) => {
+	if (!err) return;
 	if (process.env.NODE_DEBUG === pkg.name) console.error(err)
 	else console.error(err.message || (err + ''))
 	process.exit(1)
@@ -38,6 +39,9 @@ const showError = (err) => {
 
 const createClient = require('fasp-client')
 const withLocalFiles = require('fasp-client/with-local-files')
+const pump = require('pump')
+const split = require('split2')
+const {Writable} = require('stream')
 
 const readConfig = require('./lib/read-config')
 const promptWhichReceiver = require('./lib/prompt-which-receiver')
@@ -45,23 +49,46 @@ const writeConfig = require('./lib/write-config')
 const findReceiverAddress = require('./lib/find-receiver-address')
 const createUi = require('./lib/ui')
 
+const queueMode = argv._[0] === 'queue'
+
 const main = async () => {
 	let cfg = (await readConfig()) || {}
-	if (argv.receiver) {
-		cfg.receiver = argv.receiver
-	} if (argv.scan || !cfg || !cfg.receiver) {
+	if (argv.receiver) cfg.receiver = argv.receiver
+
+	if (!queueMode && (argv.scan || !cfg || !cfg.receiver)) {
 		cfg.receiver = await promptWhichReceiver()
 		await writeConfig(cfg)
 	}
 
 	const address = await findReceiverAddress(cfg.receiver)
 
-	const onProp = (prop, val) => {
-		ui.setProp(prop, val)
-	}
+	let setProp = () => {}
+	const onProp = (prop, val) => setProp(prop, val)
 	const client = createClient(address, onProp)
 	withLocalFiles(client)
-	const ui = createUi(client)
+
+	if (queueMode) {
+		pump(
+			process.stdin,
+			split(),
+			new Writable({
+				objectMode: true,
+				write: (url, _, cb) => {
+					client.queue(url) // todo: listen for success?
+					console.info('queued', url)
+					cb()
+				},
+				final: (cb) => {
+					// todo: close client nicely
+					process.exit()
+				}
+			}),
+			showError
+		)
+	} else {
+		const ui = createUi(client)
+		setProp = ui.setProp.bind(ui)
+	}
 }
 
 main()
